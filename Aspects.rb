a6bd00818988eb548bdee8160831f437afff2661
maadlog <martin.loguancio@gmail.com>
2015-09-01 11:08:42 -0300

    Aspects Class, first approach to structure.

---
+++ Aspects.rb
@@ -0,0 +1,45 @@
+class Aspects
+
+  #Se usa Aspects.on elemento1,elemento2,elemento3 do .... end
+  def on(*elements, &aspects_to_add)
+
+    elements each do |element|
+      __on__(element, &aspects_to_add)
+    end
+
+  end
+
+  #Private ?? (?)
+
+  def __on__(element, &aspects_to_add)
+
+    if (element.class) == Regexp #|| (element.class) == String Si el elemento es una expresion regular en formato no ruby
+      on_parseable_object(element,&aspects_to_add)
+    else
+      on_object(element, &aspects_to_add)
+    end
+
+  end
+
+  def on_parseable_object(expression, &aspects_to_add)
+
+    found_objects = parse_and_search_for(expression)
+    #Por ahi aca se podria llamar una recursividad al metodo "on" pelado en lugar del each, pero no se si conviene
+    #Haria un chequeo de nuevo al pedo para ver si son regExp.
+    found_objects each do |object|
+      on_object(object,&aspects_to_add)
+    end
+
+  end
+
+  def on_object(object, &aspects_to_add)
+    #TO_DO
+    #Agrega los aspectos, que vienen en un bloque
+  end
+
+  def parse_and_search_for(expression)
+    #TO_DO
+    #Devuelve una coleccion de objetos a los que hay que agregarle los aspectos
+  end
+
+end
