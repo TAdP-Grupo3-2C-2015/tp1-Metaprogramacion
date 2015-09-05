86269a7480aaf5a8521ea05502e902f35d75ecec
Charlyzzz <erwincdl@gmail.com>
2015-09-05 15:30:53 -0300

    El filtro de mensajes ahora incluye tanto privados como publicos

--- src/filters/abstract_filter.rb
+++ src/filters/abstract_filter.rb
@@ -1,8 +1,30 @@
 class AbstractFilter
 
-  def parse_symbols(symbols, origin)
-    symbols.map { |symbol| origin.instance_method(symbol) }
+  attr_accessor :origin
 
+  def matching_methods(methods)
+    methods.map { |method| origin.methods.include?(method) ?
+        self.parse_public_selector(method) : self.parse_private_selector(method) }
+
+  end
+
+  def parse_public_selector(selector)
+    origin.instance_method(selector)
+  end
+
+  def parse_private_selector(selector)
+    self.make_public(selector)
+    method = self.parse_public_selector(selector)
+    self.make_private(selector)
+    method
+  end
+
+  def make_public(selector)
+    origin.send(:public, selector)
+  end
+
+  def make_private(selector)
+    origin.send(:private, selector)
   end
 
 end
--- src/filters/name_filter.rb
+++ src/filters/name_filter.rb
@@ -7,7 +7,24 @@ class NameFilter<AbstractFilter
   end
 
   def match(origin)
-    parse_symbols(origin.instance_methods.select { |method_name| @regex_criteria =~ method_name },origin)
+    self.origin = origin
+    self.matching_methods(self.matching_selectors)
   end
 
+  def matching_selectors
+    self.all_methods.select{|selector| selector =~ @regex_criteria}
+  end
+
+  def all_methods
+    self.public_selectors + self.private_selectors
+  end
+
+  def public_selectors
+    @origin.instance_methods
+
+  end
+
+  def private_selectors
+    @origin.private_instance_methods
+  end
 end
